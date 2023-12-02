ca6326d2b536b8e49a1aade200cc22f9db3597a9
JarneSimons <r0901961@student.thomasmore.be>
2023-12-02 15:38:11 +0100

    feat: add get request api

--- controllers/api/v1/sneakers.js
+++ controllers/api/v1/sneakers.js
@@ -10,21 +10,33 @@ const sneakersSchema = new Schema({
 const Sneakers = mongoose.model('Sneakers', sneakersSchema);
 
 
-const getSneakers = (req, res) => {
-    // res.send('Get sneakers')
-    res.json({
-        "status": "success",
-        "message": "GETTING sneakers",
-    })
-}
+const getSneakers = async (req, res) => {
+    try {
+        const sneakers = await Sneakers.find({ user: "Jarne Simons" });
+
+        res.json({
+            "status": "success",
+            "data": {
+                "sneakers": sneakers
+            }
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            "status": "error",
+            "message": "Failed to get sneakers",
+            "error": err
+        });
+    }
+};
 
 const postSneakers = async (req, res) => {
     try {
         let sneakers = new Sneakers();
-        sneakers.name = "SWEAR sneaker london";
-        sneakers.size = 40;
-        sneakers.user = "Jarne Simons";
-        sneakers.color = "white";
+        sneakers.name = req.body.name;
+        sneakers.size = req.body.size;
+        sneakers.user = req.body.user;
+        sneakers.color = req.body.color;
 
         const savedSneakers = await sneakers.save();
 
