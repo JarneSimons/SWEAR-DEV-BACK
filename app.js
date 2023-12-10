e92f1e6b4029c7796f1d3b03f167b512327255ac
zzahraJ <r0883814@student.thomasmore.be>
2023-12-10 16:53:32 +0100

    fix: add the correct routes

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ const path = require('path');
 //const logger = require('morgan');
 const cors = require("cors");
 //const cookieParser = require('cookie-parser');
-const passport = require('passport');
+const passport = require('./passport/passport');
 const getRawBody = require('raw-body');
 const bodyParser = require('body-parser');
+
 
 //routers
 const indexRouter = require('./routers/index');
 const usersRouter = require('./routers/users');
-const sneakersRouter = require('.routers/api/v1/sneakers');
+const sneakersRouter = require('./routers/api/v1/sneakers');
 
 //mongoose connection
 const mongoose = require('mongoose')
@@ -34,18 +35,20 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(cors());
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(express.json);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({limit: '50mb'}));
 
-app.use(cookieParser());
+//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/v1/sneakers', sneakersRouter);
+app.use('/api/v1/sneakers', sneakersRouter, passport.authenticate('jwt',{
+  session: false
+}), sneakersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
